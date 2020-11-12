1142dc162793b8be28a50e531a82484ff9b95f10
Pavlo Borodin <p.e.borodin@gmail.com>
2020-11-12 14:09:48 +0200

    Initial commit

---
+++ Project-Lab_1/Lab_1/Lab_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input x (-1 < x < 1): ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            if (x < 1 && x > -1)
+            {
+                Console.Write($"ctg(x) - arccos(x^3) = { 1.0 / Math.Tan(x) - Math.Acos(Math.Pow(x, 3))} \n");
+            }
+            else Console.WriteLine("Your number is incorrect!");
+        }
+    }
+}
