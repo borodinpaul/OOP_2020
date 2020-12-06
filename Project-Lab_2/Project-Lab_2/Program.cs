769618e9e42e20a7c608b66b4fc75f2cf2f891ab
Pavlo Borodin <p.e.borodin@gmail.com>
2020-12-06 21:12:05 +0200

    Lab_2

---
+++ Project-Lab_2/Project-Lab_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Project_Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cylinder C = new Cylinder(5, 10);
+            Console.WriteLine("Радіус циліндра = {0,5:f3}", C.GetRadius());
+            Console.WriteLine("Висота циліндра = {0,5:f3}", C.GetHeight());
+            Console.WriteLine("Площа бічної поверхні циліндра = {0,5:f3}", C.AreaSideSurface());
+            Console.WriteLine("Площа основи циліндра = {0,5:f3}", C.AreaBase());
+            Console.WriteLine("Об'єм циліндра = {0,5:f3}", C.Volume());
+            C.SetRadius(10);
+            Console.WriteLine("Радіус циліндра = {0,5:f3}", C.GetRadius());
+        }
+    }
+}
---
+++ Project-Lab_2/Project-Lab_2/Сylinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Lab_2
+{
+    class Cylinder
+    {
+        double radius, height;
+        public Cylinder(double r, double h) { 
+            radius = r; 
+            height = h; 
+        }
+        ~Cylinder()
+        {
+            Console.WriteLine("Об'єкт зруйновано");
+        }
+        public double AreaBase()
+        {
+            return radius * Math.Pow(Math.PI, 2);
+        }
+        public double AreaSideSurface() 
+        { 
+            return 2 * Math.PI * radius * height; 
+        }
+
+        public double Volume() 
+        { 
+            return Math.PI* Math.Pow(radius, 2) * height; 
+        }
+
+        public void SetRadius(double r) 
+        { 
+            radius = r; 
+        }
+        public void SetHeight(double h) 
+        { 
+            height = h; 
+        }
+        public double GetRadius() 
+        { 
+            return radius; 
+        }
+        public double GetHeight() 
+        { 
+            return height; 
+        }
+    }
+}
